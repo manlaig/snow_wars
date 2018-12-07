9d142c215982c8154f45d52eab04c1a2c9fee0da
Michael Ganzorig <manlai1025@gmail.com>
2018-12-06 16:07:23 -0800

    production tiles material code

---
+++ Assets/Scripts/BuildingProduction/ProductionTile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ProductionTile : MonoBehaviour
+{
+    [SerializeField] Material tileMaterial;
+	void Start ()
+    {
+        //tileMaterial.color = Color.green;
+        tileMaterial.SetColor("_TintColor", Color.green);
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Environment"))
+            tileMaterial.color = Color.red;
+        //else
+            //GetComponent<MeshRenderer>().material.color = Color.green;
+    }
+}
---
+++ Assets/Scripts/BuildingProduction/SpawnBuildings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBuildings : MonoBehaviour
+{
+    [SerializeField] GameObject productionTile;
+
+	void Start ()
+    {
+        if (!productionTile)
+            Debug.LogError("Production Tile is NULL");
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
