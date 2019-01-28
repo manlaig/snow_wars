14f8731927489f794f4d690553c9d2eb7387d877
Michael Ganzorig <manlai1025@gmail.com>
2019-01-28 11:22:10 -0800

    worker building queue

--- Assets/Scripts/BuildingProduction/BuilderUnit.cs
+++ Assets/Scripts/BuildingProduction/BuilderUnit.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,12 +9,23 @@ public class BuilderUnit : MonoBehaviour
     NavMeshAgent agent;
     GameObject currentBuildingToBuild;
     Animation animation;
-
+    Queue<GameObject> buildingsQ;
+    bool idle;
 
     void Start()
     {
+        idle = true;
+        buildingsQ = new Queue<GameObject>();
         agent = GetComponent<NavMeshAgent>();
         animation = GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+        if(buildingsQ.Count > 0 && idle)
+        {
+            WorkOnBuilding(buildingsQ.Dequeue());
+        }
     }
 
     void OnEnable()
@@ -32,16 +42,29 @@ public class BuilderUnit : MonoBehaviour
 
     void NewBuildingPlaced(GameObject building)
     {
-        currentBuildingToBuild = building;
-        agent.SetDestination(building.transform.position);
-        if(animation)
-            animation.Play(animation.clip.name.Split('|')[0] + "|Run");
+        /*
+         * Later on, when we'll have multiple workers, use a different class 
+         * to find the closest worker and enqueue the building to it
+         */
+        buildingsQ.Enqueue(building);
+    }
+
+    void WorkOnBuilding(GameObject go)
+    {
+        currentBuildingToBuild = go;
+        agent.SetDestination(go.transform.position);
+        idle = false;
+        PlayAnimation("Run");
+
     }
 
     void FinishedBuilding(GameObject go)
     {
-        Debug.Log("Finished building");
         agent.isStopped = false;
+        agent.ResetPath();
+        PlayAnimation("Idle");
+
+        idle = true;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -53,20 +76,19 @@ public class BuilderUnit : MonoBehaviour
             ShowBuildProgress progress = currentBuildingToBuild.GetComponent<ShowBuildProgress>();
             if (progress)
             {
-                Debug.Log("Reached building to build");
                 progress.StartBuilding();
 
-                ResetAgent();
                 agent.isStopped = true;
+                PlayAnimation("Gather");
+
+                currentBuildingToBuild = null;
             }
-            currentBuildingToBuild = null;
         }
     }
 
-    void ResetAgent()
+    void PlayAnimation(string name)
     {
-        agent.ResetPath();
         if (animation)
-            animation.Play(animation.clip.name.Split('|')[0] + "|Idle");
+            animation.Play(animation.clip.name.Split('|')[0] + "|" + name);
     }
 }
