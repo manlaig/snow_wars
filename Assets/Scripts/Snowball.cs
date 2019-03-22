1be5d1c3f9a898bf4b9253026ddb0eec181e0750
Michael Ganzorig <manlai1025@gmail.com>
2019-03-22 12:36:43 -0700

    adding snowball script

---
+++ Assets/Scripts/Snowball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Snowball : MonoBehaviour
+{
+    [SerializeField] GameObject explode_effect;
+    [SerializeField] float speed = 3f;
+
+    [HideInInspector] public Transform target;
+    // the ControlRange script specifies the damage to deal on the target
+    [HideInInspector] public float damage = 10f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == target.gameObject)
+        {
+            // destroy the snowball explode affect after playing
+            Destroy(Instantiate(explode_effect, transform.position, transform.rotation), 1f);
+            target.gameObject.GetComponent<ControlBasic>().GetHit(damage, gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    void Update ()
+    {
+		if(target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed / 10f);
+        }
+	}
+}
